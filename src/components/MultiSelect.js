ada8778de1ad0a48a40ad13aef9ff6f4001e041e
mikaylawerzanski <werzanski.m@northeastern.edu>
2021-04-12 19:47:30 -0400

    BT-7 add single player button and dispatch

--- src/components/MultiSelect.js
+++ src/components/MultiSelect.js
@@ -1,11 +1,23 @@
 import { selectMultiplayerMode } from "../client";
+import { useDispatch } from "react-redux";
+import {selectSinglePlayer} from "../redux/gameStateActions"
 
 const MultiSelect = () => {
+    const dispatch = useDispatch();
+
+    const selectSinglePlayerClick = () => {
+        dispatch(selectSinglePlayer())
+    }
+
+    // const selectMultiPlayerClick = () => {
+    //     dispatch(selectMultiPlayer())
+    // }
+
   return (
     <>
       <p>Choose single player or multiplayer.</p>
-      <button onClick={selectMultiplayerMode}>Multiplayer</button>
-      <button>Single Player</button>
+      <button className='btn-sm btn-primary' onClick={selectMultiplayerMode}>Multiplayer</button>
+      <button className='btn-sm btn-primary' onClick={() => selectSinglePlayerClick()}>Single Player</button>
     </>
   );
 };

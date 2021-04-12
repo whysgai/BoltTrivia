4ff12bf89882f70601aeddc0327e6b72b87c2a30
Will Cohen <whysgai@gmail.com>
2021-04-11 21:10:35 -0400

    BT-32 feat: Add some initial state values

--- src/redux/storeConstants.js
+++ src/redux/storeConstants.js
@@ -6,11 +6,18 @@ export const PLAYER_MODE = {
 export const GAME_MODE = {
     TIME_MODE = "TIME_MODE",
     SCORE_MODE = "SCORE_MODE"
-}
+};
 
 export const GAME_PHASE = {
     SELECT_MODE = "SELECT_MODE",
     SET_CONFIGsS = "SET_CONFIGS",
     PLAY_GAME = "PLAY_GAME",
     VIEW_SCORES = "VIEW_SCORES"
-}
+};
+
+export const INITIAL_STATE = {
+    player : null,
+    mode : null,
+    phase : GAME_PHASE.SELECT_MODE,
+    questions : []
+};

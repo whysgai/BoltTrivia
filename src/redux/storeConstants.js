08e0d504f337f273a2604f3785f71366ca7b3631
mikaylawerzanski <werzanski.m@northeastern.edu>
2021-04-12 17:12:11 -0400

    BT-6: update game phases

--- src/redux/storeConstants.js
+++ src/redux/storeConstants.js
@@ -1,23 +1,18 @@
 export const PLAYER_MODE = {
-    SINGLE_PLAYER = "SINGLE_PLAYER",
-    MULTI_PLAYER = "MULTI_PLAYER"
+    SINGLE_PLAYER: "SINGLE_PLAYER",
+    MULTI_PLAYER: "MULTI_PLAYER"
 };
 
-export const GAME_MODE = {
-    TIME_MODE = "TIME_MODE",
-    SCORE_MODE = "SCORE_MODE"
+export const GAME_TYPE = {
+    TIME_MODE: "TIME_MODE",
+    SCORE_MODE: "SCORE_MODE"
 };
 
 export const GAME_PHASE = {
-    SELECT_MODE = "SELECT_MODE",
-    SET_CONFIGS = "SET_CONFIGS",
-    PLAY_GAME = "PLAY_GAME",
-    VIEW_SCORES = "VIEW_SCORES"
-};
-
-export const INITIAL_STATE = {
-    player : null,
-    mode : null,
-    phase : GAME_PHASE.SELECT_MODE,
-    questions : []
+    SELECT_MULTI: "SELECT_MULTI",
+    SELECT_PLAYER: "SELECT_PLAYER",
+    SELECT_GAME_TYPE: "SELECT_GAME_TYPE",
+    SET_CONFIGS: "SET_CONFIGS",
+    PLAY_GAME: "PLAY_GAME",
+    VIEW_SCORES: "VIEW_SCORES"
 };

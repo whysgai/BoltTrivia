593ced936e48d4c335d92253c8a6b042779525f7
Will Cohen <whysgai@gmail.com>
2021-04-11 20:13:05 -0400

    BT-32 feat: Add preliminary store constants

---
+++ src/redux/storeConstants.js
@@ -0,0 +1,16 @@
+export const PLAYER_MODE = {
+    SINGLE_PLAYER = "SINGLE_PLAYER",
+    MULTI_PLAYER = "MULTI_PLAYER"
+};
+
+export const GAME_MODE = {
+    TIME_MODE = "TIME_MODE",
+    SCORE_MODE = "SCORE_MODE"
+}
+
+export const GAME_PHASE = {
+    SELECT_MODE = "SELECT_MODE",
+    SET_CONFIGsS = "SET_CONFIGS",
+    PLAY_GAME = "PLAY_GAME",
+    VIEW_SCORES = "VIEW_SCORES"
+}

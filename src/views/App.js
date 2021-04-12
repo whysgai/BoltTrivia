5b37d72f5fb6a8335b7cbb0a5b99d11f84011321
mikaylawerzanski <werzanski.m@northeastern.edu>
2021-04-12 17:11:41 -0400

    BT-6: add multi select component

--- src/views/App.js
+++ src/views/App.js
@@ -1,10 +1,18 @@
 import "../styles/App.css";
-import { selectMultiplayerMode } from "../client";
+import {GAME_PHASE} from "../redux/storeConstants"
+import { useSelector } from "react-redux";
+import MultiSelect from "../components/MultiSelect";
 
 function App() {
+  const gameState = useSelector(state => state.gameStateReducer)
+
   return (
     <div className="App">
-      <button onClick={selectMultiplayerMode}>test</button>
+      <h2>Bolt Trivia!</h2>
+      {
+        gameState.phase === GAME_PHASE.SELECT_MULTI &&
+        <MultiSelect/>
+      }
     </div>
   );
 }

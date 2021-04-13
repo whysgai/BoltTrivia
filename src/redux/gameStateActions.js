3c0d5b6afbc2459377eca7021ed551ba6ce80357
mikaylawerzanski <werzanski.m@northeastern.edu>
2021-04-13 14:03:56 -0400

    BT-9 move doc to actions folder

--- src/redux/gameStateActions.js
+++
@@ -1,9 +0,0 @@
-import { PLAYER_MODE, GAME_TYPE, GAME_PHASE } from "./storeConstants";
-
-export const selectSinglePlayer = () => ({
-    type: PLAYER_MODE.SINGLE_PLAYER
-})
-
-export const selectMultiPlayer = () => ({
-    type: PLAYER_MODE.MULTI_PLAYER
-})

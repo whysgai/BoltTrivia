df78acf95d9a53dc52ec4d6c684d1825cc8d9129
mikaylawerzanski <werzanski.m@northeastern.edu>
2021-04-12 20:43:19 -0400

    BT-9 add multiplayer button triggers

--- src/components/MultiSelect.js
+++ src/components/MultiSelect.js
@@ -1,22 +1,23 @@
 import { selectMultiplayerMode } from "../client";
 import { useDispatch } from "react-redux";
-import {selectSinglePlayer} from "../redux/gameStateActions"
+import {selectSinglePlayer, selectMultiPlayer} from "../redux/gameStateActions"
 
 const MultiSelect = () => {
     const dispatch = useDispatch();
 
     const selectSinglePlayerClick = () => {
-        dispatch(selectSinglePlayer())
+        dispatch(selectSinglePlayer());
     }
 
-    // const selectMultiPlayerClick = () => {
-    //     dispatch(selectMultiPlayer())
-    // }
+    const selectMultiPlayerClick = () => {
+        dispatch(selectMultiPlayer());
+        selectMultiplayerMode();
+    }
 
   return (
     <>
       <p>Choose single player or multiplayer.</p>
-      <button className='btn-sm btn-primary' onClick={selectMultiplayerMode}>Multiplayer</button>
+      <button className='btn-sm btn-primary' onClick={() => selectMultiPlayerClick()}>Multiplayer</button>
       <button className='btn-sm btn-primary' onClick={() => selectSinglePlayerClick()}>Single Player</button>
     </>
   );

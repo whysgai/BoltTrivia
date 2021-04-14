dfc609d9f094c972d97bdfa3587abfa04e006712
Will Cohen <whysgai@gmail.com>
2021-04-13 22:31:08 -0400

    BT19 feat: Call client on button click

--- src/components/GameTypeSelect.js
+++ src/components/GameTypeSelect.js
@@ -1,9 +1,30 @@
+
+import { GAME_TYPE } from "../redux/storeConstants";
+import { selectGameType } from "../client";
+import { useSelector } from "react-redux";
+
 const GameTypeSelect = () => {
+    const selectedType = useSelector(state => state.gameStateReducer.type);
+
+    const selectTimed = () => {
+        selectGameType(GAME_TYPE.TIME_MODE);
+    };
+
+    const selectScore = () => {
+        selectGameType(GAME_TYPE.SCORE_MODE);
+    };
+
     return (
         <>
             <p>Player 1: select a game mode:</p>
-            <button className="btn btn-info">Timed</button>
-            <button className="btn btn-warning">Score</button>
+            <button className="btn btn-info" onClick={() => selectTimed()}>Timed</button>
+            <button className="btn btn-warning" onClick={() => selectScore()}>Score</button>
+            {
+                selectedType !== null ?
+                    <p>You chose game type {selectedType}</p>
+                    :
+                    <></>
+            }
         </>
     );
 };

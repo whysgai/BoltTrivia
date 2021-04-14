ba0f2b64c36d4f280e712e56217e93d7c0339284
Will Cohen <whysgai@gmail.com>
2021-04-13 21:48:11 -0400

    BT19 refactor: Rename component

---
+++ src/components/GameTypeSelect.js
@@ -0,0 +1,11 @@
+const GameTypeSelect = () => {
+    return (
+        <>
+            <p>Player 1: select a game mode:</p>
+            <button className="btn btn-info">Timed</button>
+            <button className="btn btn-warning">Score</button>
+        </>
+    );
+};
+
+export default GameTypeSelect;

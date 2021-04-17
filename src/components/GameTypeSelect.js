c339654092607ba4a339b5672237cd2a66de62c6
Will Cohen <whysgai@gmail.com>
2021-04-16 20:55:30 -0400

    BT29 feat: Move temporary selected type to local state

--- src/components/GameTypeSelect.js
+++ src/components/GameTypeSelect.js
@@ -1,6 +1,7 @@
+import {useState, useEffect} from 'react';
 import { GAME_TYPE } from "../redux/storeConstants";
 // import { selectGameType } from "../client";
-import { gameTypeSelection } from "../redux/actions/gameStateActions";
+//import { gameTypeSelection } from "../redux/actions/gameStateActions";
 import { useSelector, useDispatch } from "react-redux";
 import WaitingScreen from "./WaitingScreen";
 import TimedConfigs from "./TimedConfigs";
@@ -8,24 +9,28 @@ import ScoreConfigs from "./ScoreConfigs";
 
 
 const GameTypeSelect = () => {
-  const selectedType = useSelector((state) => state.gameStateReducer.type);
+  //const selectedType = useSelector((state) => state.gameStateReducer.type);
   const playerNumber = useSelector((state) => state.gameStateReducer.player);
   const playerAvailability = useSelector(
     (state) => state.gameStateReducer.playerAvailability
   );
+
+  const [selectedType, setType] = useState();
 
   const dispatch = useDispatch();
 
   const selectTimed = () => {
     console.log("Setting to", GAME_TYPE.TIME_MODE);
     // selectGameType(GAME_TYPE.TIME_MODE);
-    dispatch(gameTypeSelection(GAME_TYPE.TIME_MODE));
+    //dispatch(gameTypeSelection(GAME_TYPE.TIME_MODE));
+    setType(GAME_TYPE.TIME_MODE);
   };
 
   const selectScore = () => {
     console.log("Setting to", GAME_TYPE.SCORE_MODE);
     //selectGameType(GAME_TYPE.SCORE_MODE);
-    dispatch(gameTypeSelection(GAME_TYPE.SCORE_MODE));
+    //dispatch(gameTypeSelection(GAME_TYPE.SCORE_MODE));
+    setType(GAME_TYPE.SCORE_MODE);
   };
 
   return (

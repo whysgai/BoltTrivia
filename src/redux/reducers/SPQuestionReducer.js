95e533d7e1b180d4f3d061331c82752a81e1bb60
mikaylawerzanski <werzanski.m@northeastern.edu>
2021-04-17 16:09:20 -0400

    BT-12 add addAnswer state change

--- src/redux/reducers/SPQuestionReducer.js
+++ src/redux/reducers/SPQuestionReducer.js
@@ -1,16 +1,27 @@
 import {  } from "../storeConstants";
-import { SET_SP_TRIVIA_QUESTIONS } from "../actionConstants"
+import { SET_SP_TRIVIA_QUESTIONS, ADD_SP_ANSWER } from "../actionConstants"
 
 const INITIAL_STATE = {
   questions: [],
+  count: 10,
+  answeredCount: 0,
+  answers: [],
 };
 
-export const questionReducer = (state = INITIAL_STATE, action) => {
+export const SPQuestionReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case SET_SP_TRIVIA_QUESTIONS:
-      console.log('questions: ', action.payload.questions)
+      return { ...state,
+        questions: action.payload.questions,
+        count: action.payload.questions.length,
+      }
+    case ADD_SP_ANSWER:
+      const newAnsweredCount = state.answeredCount + 1;
+      const newAnswers = state.answers.concat([action.payload.answer])
       return {
-        questions: action.payload.questions
+        ...state,
+        answeredCount: newAnsweredCount,
+        answers: newAnswers
       }
     default:
       return state;

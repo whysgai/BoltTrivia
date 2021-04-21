246d5a1e74f7a1b0b37345db54ed5f56a2ed3f95
Will Cohen <whysgai@gmail.com>
2021-04-21 18:23:10 -0400

    BT50 feat: Add placeholders for scoreboard sections

--- src/components/MPScoreboardTimed.js
+++ src/components/MPScoreboardTimed.js
@@ -1,8 +1,13 @@
+import MPTimer from "./MPTimer";
 
 const MPScoreboardTimed = () => {
     return (
         <div>
             <p>Scoreboard (timed)</p>
+            <p>Timer Component</p>
+            <MPTimer />
+            <p>List of player's questions</p>
+            <p>Opponent's ratio</p>
         </div>
     );
 };

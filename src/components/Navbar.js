633805587090b5aae0d15cbbf470c508a7f83dfc
mikaylawerzanski <werzanski.m@northeastern.edu>
2021-04-20 20:01:58 -0400

    BT-41 add bool to restartGame action to determine error message appearance

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import {useSelector, useDispatch} from "react-redux";
 import {selectRestart} from "../client"
-import {PLAYER_MODE} from "../redux/storeConstants"
+import {PLAYER_MODE, GAME_PHASE} from "../redux/storeConstants"
 import { restartGame } from '../redux/actions/gameStateActions';
 
 const NavbarComponent = () => {
     const dispatch = useDispatch();
     const multiSelect = useSelector(state => state.gameStateReducer.multiSelect)
+    const phase = useSelector(state => state.gameStateReducer.phase)
 
     const restart = () => {
         if (multiSelect === PLAYER_MODE.SINGLE_PLAYER) {
-            dispatch(restartGame())
+            dispatch(restartGame(true))
+        } else if (phase === GAME_PHASE.SELECT_MULTI) {
+            dispatch(restartGame(false))
         } else {
             selectRestart()
         }

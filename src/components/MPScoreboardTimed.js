f86d95b20bda52eb6c25a261f3f0a33b1cc5c0d5
Will Cohen <whysgai@gmail.com>
2021-04-21 18:56:57 -0400

    BT50 feat: Show timer and conditionally display players

--- src/components/MPScoreboardTimed.js
+++ src/components/MPScoreboardTimed.js
@@ -1,13 +1,16 @@
+import { useSelector } from "react-redux";
 import MPTimer from "./MPTimer";
 
 const MPScoreboardTimed = () => {
+    const player = useSelector(state => state.gameStateReducer.player);
+
     return (
         <div>
             <p>Scoreboard (timed)</p>
             <p>Timer Component</p>
             <MPTimer />
-            <p>List of player's questions</p>
-            <p>Opponent's ratio</p>
+            <p>List of {player === 0 ? "P1" : "P2"}'s questions</p>
+            <p>{player === 0 ? "P2" : "P1"}'s ratio</p>
         </div>
     );
 };

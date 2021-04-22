3f3a5197a8b94d8ecc4964500dd6ce1fc6b8f7a2
Will Cohen <whysgai@gmail.com>
2021-04-21 20:48:17 -0400

    BT50 feat: Display wins and losses

--- src/components/MPScoreboardTimed.js
+++ src/components/MPScoreboardTimed.js
@@ -3,6 +3,8 @@ import MPTimer from "./MPTimer";
 
 const MPScoreboardTimed = () => {
     const player = useSelector(state => state.gameStateReducer.player);
+    //dummy score history
+    const wins = [true, false, true, true, false, true];
 
     return (
         <div>
@@ -10,6 +12,13 @@ const MPScoreboardTimed = () => {
             <p>Timer Component</p>
             <MPTimer />
             <p>List of {player === 0 ? "P1" : "P2"}'s questions</p>
+            <div>
+                {
+                    wins.map((question, index) => 
+                        <div>Question {index}: {question ? "Correct" : "Wrong"}</div>
+                    )
+                }
+            </div>
             <p>{player === 0 ? "P2" : "P1"}'s ratio</p>
         </div>
     );

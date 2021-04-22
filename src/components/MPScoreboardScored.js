97ccb26b4cbf554f8c8bafd81cf865e709f434dd
Will Cohen <whysgai@gmail.com>
2021-04-22 13:33:12 -0400

    BT50 feat: Show question goal for score mode

--- src/components/MPScoreboardScored.js
+++ src/components/MPScoreboardScored.js
@@ -4,6 +4,7 @@ import MPTimer from "./MPTimer";
 const MPScoreboardScored = () => {
     const player = useSelector(state => state.gameStateReducer.player);
     const scores = useSelector(state => state.MPQuestionReducer.scores);
+    const goal = useSelector(state => state.gameStateReducer.configs.questionCount);
     //dummy score history
     const wins = [true, false, true, true, false, true];
 
@@ -13,7 +14,7 @@ const MPScoreboardScored = () => {
                      
             <p>{player === 0 ? "P1" : "P2"}'s score: {scores[player]}</p>
             <p>{player === 0 ? "P2" : "P1"}'s score: {scores[player === 0 ? 1 : 0]}</p>
-            <p>Goal: </p>
+            <p>Out of&nbso;{goal} </p>
             <div>
                 {
                     wins.map((question, index) => 

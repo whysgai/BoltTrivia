afa6bde344a21ff3e8bc5bf44959ef87d913258d
mikaylawerzanski <werzanski.m@northeastern.edu>
2021-04-24 16:04:50 -0400

    BT-63 remove testing data from MPResults

--- src/components/MPResults.js
+++ src/components/MPResults.js
@@ -3,54 +3,12 @@ import MPResultsCard from "./MPResultsCard";
 
 
 const MPResults = () => {
-    // const questions = useSelector(state => state.MPQuestionReducer.questions);
-    // const answerStrings = useSelector(state => state.MPQuestionReducer.answerStrings);
-    // const playerAnswers = useSelector(state => state.MPQuestionReducer.playerAnswers);
-    // const player = useSelector(state => state.gameStateReducer.player);
-    // const winner = useSelector(state => state.MPQuestionReducer.winner);
-    // const scores = useSelector(state => state.MPQuestionReducer.scores);
-
-    //FAKE DATA FOR TESTING
-    const questions = [
-        {
-            category: "Entertainment: Books",
-            correct_answer: "Hans Christian Andersen",
-            difficulty: "medium",
-            incorrect_answers: ["Charles Dickens", "Lewis Carroll", "Oscar Wilde"],
-            question: "Who wrote the children&#039;s story &quot;The Little Match Girl&quot;?",
-            type: "multiple",
-            allAnswers: ["Charles Dickens", "Hans Christian Andersen", "Lewis Carroll", "Oscar Wilde"]
-        },
-        {category: "Entertainment: Music",
-        correct_answer: "White",
-        difficulty: "medium",
-        incorrect_answers: (3) ["Black", "Blue", "Yellow"],
-        question: "EDM producer Marshmello performs live wearing clothes and a marshmallow mask of what colour?",
-        type: "multiple",
-        allAnswers: ["Black", "Blue", "White", "Yellow"]
-        },
-        {category: "Science & Nature",
-        correct_answer: "Sputnik 1",
-        difficulty: "easy",
-        incorrect_answers: (3) ["Soyuz 7K-OK", "Zenit-2", "Voskhod 3KV"],
-        question: "What was the name of the first artificial Earth satellite, launched by the Soviet Union in 1957?",
-        type: "multiple",
-        allAnswers: ["Sputnik 1", "Soyuz 7K-OK", "Zenit-2", "Voskhod 3KV"]},
-        {category: "General Knowledge",
-        correct_answer: "London Liverpool Street",
-        difficulty: "medium",
-        incorrect_answers: (3) ["Frankfurt (Main) Hauptbahnhof", "Paris Gare du Nord", "Brussels Midi"],
-        question: "Amsterdam Centraal station is twinned with what station?",
-        type: "multiple",
-        allAnswers: ["Frankfurt (Main) Hauptbahnhof", "London Liverpool Street", "Paris Gare du Nord", "Brussels Midi"]}
-    ]
-    const answerStrings = ["Charles Dickens", "White", "Soyuz 7K-OK", "London Liverpool Street"]
-    const playerAnswers = [[false, true, false, true], [true, true, false, true]]
-    const player = 0;
-    const winner = 'P2'
-    const scores = [2,3]
-
-
+    const questions = useSelector(state => state.MPQuestionReducer.questions);
+    const answerStrings = useSelector(state => state.MPQuestionReducer.answerStrings);
+    const playerAnswers = useSelector(state => state.MPQuestionReducer.playerAnswers);
+    const player = useSelector(state => state.gameStateReducer.player);
+    const winner = useSelector(state => state.MPQuestionReducer.winner);
+    const scores = useSelector(state => state.MPQuestionReducer.scores);
 
     const gameOpponent = () => {
         if (player === 0) {

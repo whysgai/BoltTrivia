4012327315c26494f4d0775b666d60beb6aa5936
mikaylawerzanski <werzanski.m@northeastern.edu>
2021-04-23 21:20:19 -0400

    move results page changes to BT-63

--- src/components/MPResults.js
+++ src/components/MPResults.js
@@ -1,10 +1,15 @@
+import { useSelector } from "react-redux";
 
 
 const MPResults = () => {
+    const questions = useSelector(state => state.MPQuestionReducer.questions);
+    const answerStrings = useSelector(state => state.MPQuestionReducer.answerStrings);
+    const playerAnswers = useSelector(state => state.MPQuestionReducer.playerAnswers);
+
     return (
-        <>
+        <div className="card-group">
             <h3>RESULTS</h3>
-        </>
+        </div>
     )
 }
 
---
+++ src/components/MPResultsCard.js
@@ -0,0 +1,18 @@
+import { useSelector } from "react-redux";
+
+
+const MPResultsCard = (props) => {
+    const question = props.question;
+    const answerString = props.answerString;
+    const playerAnswer = props.playerAnswer;
+
+    return (
+        <div className="card">
+            <div className="card-body">
+                
+            </div>
+        </div>
+    )
+}
+
+export default MPResultsCard;

5fd974af1067a154ad04b7ae76b54aae4a56bb60
Will Cohen <whysgai@gmail.com>
2021-04-26 11:14:01 -0400

    BT-65 refactor: Move all loading scenarios to wating component

--- src/components/WaitingScreen.js
+++ src/components/WaitingScreen.js
@@ -7,13 +7,24 @@ const WaitingScreen = () => {
 
   return (
     <>
-      {gamePhase === GAME_PHASE.SELECT_GAME_TYPE && playerNumber === 1 ? (
+      {gamePhase === GAME_PHASE.SELECT_GAME_TYPE && playerNumber === 1 ? 
         <p>Waiting for Player 1 to select game mode</p>
-      ) : gamePhase === GAME_PHASE.SELECT_GAME_TYPE && playerNumber === 0 ? (
-        <p>Waiting for Player 2 to Join</p>
-      ) : (
-        <></>
-      )}
+        : 
+        gamePhase === GAME_PHASE.SELECT_GAME_TYPE && playerNumber === 0 ? 
+          <p>Waiting for Player 2 to Join</p>
+          : 
+          gamePhase === GAME_PHASE.LOADING_GAME ? 
+            <p>MP loading the game</p>
+            :
+            gameState.phase === GAME_PHASE.AWAITING_RESULTS ?
+              <p>End condition reached, waiting on server</p>
+              // Ran out of questions
+              // Ran out of time
+              // Reached goal
+              // Other player reached goal
+              :
+              <></>
+      }
     </>
   );
 };

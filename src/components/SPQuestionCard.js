383c0284ba91a2a3b24b83ceb4cbc2be8d27f784
mikaylawerzanski <werzanski.m@northeastern.edu>
2021-04-28 23:34:48 -0400

    BT-70 add single player game styling

--- src/components/SPQuestionCard.js
+++ src/components/SPQuestionCard.js
@@ -40,65 +40,133 @@ const SPQuestionCard = (props) => {
   };
 
   return (
-    <div>
-      <h4 className="pb-3 text-center">Category: {question.category}</h4>
-      <h5>Question: {characterCheck(question.question)}</h5>
-      {props.multipleChoice.map((selection, index) => (
-        <div key={index} className="form-check pb-2">
-          <input
-            required
-            className="form-check-input"
-            checked={answer === characterCheck(selection)}
-            disabled={submit}
-            type="radio"
-            name="gridRadios"
-            id={"gridRadios" + index}
-            value="type-text"
-            onChange={() => {
-              setAnswer(characterCheck(selection));
-              setValid(true);
-            }}
-          />
-          <label
-            className="form-check-label pb-2"
-            htmlFor={"gridRadios" + index}
-          >
-            {characterCheck(selection)}
-          </label>
+    <div className="mt-3 mx-3 mb-3">
+      <div className="card-subtitle sp-quiz-category">
+        <span className="pb-3 text-center">Category: {question.category}</span>
+        <span>Question # {props.answeredCount+1}</span>
+      </div>
+      <h3 className="card-title sp-quiz-question">Question: {characterCheck(question.question)}</h3>
+      <div className="seperator"/>
+    { !submit ? (
+      <div>
+        <div className="quiz-answer-list">
+          {props.multipleChoice.map((selection, index) => (
+            <div key={index} className="sp-quiz-answer">
+              <input
+                required
+                className="form-check-input btn-check"
+                checked={answer === characterCheck(selection)}
+                disabled={submit}
+                type="radio"
+                name="gridRadios"
+                id={"gridRadios" + index}
+                value="type-text"
+                onChange={() => {
+                  setAnswer(characterCheck(selection));
+                  setValid(true);
+                }}
+              />
+              <label
+                className={`form-check-label btn
+                  ${
+                    answer === characterCheck(selection) ?
+                      "btn-dark"
+                      :
+                      "btn-outline-dark"
+                  }
+                `}
+                htmlFor={"gridRadios" + index}
+              >
+                {characterCheck(selection)}
+              </label>
+            </div>
+          ))}
         </div>
-      ))}
-      {!submit ? (
-        <button
-          className="btn btn-success float-right"
-          onClick={() => updateCount()}
-          disabled={!valid}
-        >
-          Submit
-        </button>
+
+        <div className="seperator"/>
+          <button
+            className="btn btn-primary sp-quiz-submit"
+            onClick={() => updateCount()}
+            disabled={!valid}
+          >
+            <h3>SUBMIT</h3>
+          </button>
+      </div>
       ) : (
+      <div>
+        <div className="quiz-answer-list">
+        {props.multipleChoice.map((selection, index) => (
+          <div key={index} className="sp-quiz-answer">
+            <input
+              required
+              className="form-check-input btn-check"
+              checked={answer === characterCheck(selection)}
+              disabled={submit}
+              type="radio"
+              name="gridRadios"
+              id={"gridRadios" + index}
+              value="type-text"
+              onChange={() => {
+                setAnswer(characterCheck(selection));
+                setValid(true);
+              }}
+            />
+            <label
+              className={`form-check-label btn
+                ${
+                  answer === characterCheck(selection) && correctBool ?
+                    "btn-success"
+                    :
+                    (
+                      answer === characterCheck(selection) && !correctBool ?
+                      "btn-danger"
+                      :
+                      (
+                        selection === question.correct_answer ?
+                        "btn-success"
+                        :
+                        "btn-outline-dark"
+                      )
+                    )
+                }
+              `}
+              htmlFor={"gridRadios" + index}
+            >
+              {characterCheck(selection)}
+            </label>
+          </div>
+        ))}
+        </div>
+
+        <div className="seperator"/>
         <div className="text-center">
-          <h3 className="text-primary">{correct}</h3>
-          <h5 className="text-success">
-            Correct Answer: {characterCheck(question.correct_answer)}
-          </h5>
+          <h3 className={`
+                ${
+                  correctBool ?
+                    "text-success"
+                    :
+                    "text-danger"
+                }
+              `}>{correct}</h3>
           {answeredCount === totalQuestionCount - 1 ? (
             <button
-              className="btn btn-dark float-right"
+              className="btn btn-dark sp-quiz-submit"
               onClick={() => updateCount()}
             >
-              Finish
+              <h3>FINISH</h3>
             </button>
           ) : (
             <button
-              className="btn btn-dark float-right"
+              className="btn btn-dark sp-quiz-submit"
               onClick={() => updateCount()}
             >
-              Next
+              <h3>NEXT</h3>
             </button>
           )}
         </div>
-      )}
     </div>
+    )}
+  </div>
   );
 };
 
